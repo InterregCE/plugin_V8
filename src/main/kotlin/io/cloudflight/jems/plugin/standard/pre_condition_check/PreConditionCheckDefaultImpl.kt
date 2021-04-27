2f5855f26da93afcc8fe06a6c421f04a6bd8bb37
Mehran Khaksar haghani dehkordi <Mehran.Khaksarhaghanidehkordi@cloudflight.io>
2021-04-27 16:55:35 +0200

    adds a simple pre condition check logic

--- src/main/kotlin/io/cloudflight/jems/plugin/standard/pre_condition_check/PreConditionCheckDefaultImpl.kt
+++ src/main/kotlin/io/cloudflight/jems/plugin/standard/pre_condition_check/PreConditionCheckDefaultImpl.kt
@@ -5,33 +5,47 @@ import io.cloudflight.jems.plugin.contract.pre_condition_check.PreConditionCheck
 import io.cloudflight.jems.plugin.contract.pre_condition_check.models.MessageType
 import io.cloudflight.jems.plugin.contract.pre_condition_check.models.PreConditionCheckMessage
 import io.cloudflight.jems.plugin.contract.pre_condition_check.models.PreConditionCheckResult
+import io.cloudflight.jems.plugin.contract.services.ProjectDataProvider
 import org.springframework.stereotype.Service
 
 
 @Service
-open class PreConditionCheckDefaultImpl : PreConditionCheckPlugin {
+open class PreConditionCheckDefaultImpl(val projectDataProvider: ProjectDataProvider) : PreConditionCheckPlugin {
 
-    override fun check(projectId: Long): PreConditionCheckResult =
-            PreConditionCheckResult(
-                    listOf(
-                            PreConditionCheckMessage(
-                                    "external",
-                                    MessageType.ERROR,
-                                    emptyList()
-                            )
-                    ),
-                    isSubmissionAllowed = true
+    override fun check(projectId: Long): PreConditionCheckResult {
+        val projectData = projectDataProvider.getProjectDataForProjectId(projectId)
+
+        if (projectData.sectionA == null ||
+            projectData.sectionA!!.title.isEmpty() ||
+            projectData.sectionA!!.title.all { it.translation.isNullOrBlank() }
+        ) {
+            return PreConditionCheckResult(
+                listOf(
+                    PreConditionCheckMessage(
+                        "plugin.section.a.project.title.not.defined",
+                        MessageType.ERROR,
+                        emptyList()
+                    )
+                ),
+                isSubmissionAllowed = false
             )
+        }
+
+        return PreConditionCheckResult(
+            emptyList(),
+            isSubmissionAllowed = true
+        )
+    }
 
     override fun getDescription(): String =
-            "Standard implementation for pre condition check"
+        "Standard implementation for pre condition check"
 
     override fun getKey() =
-            "standard-pre-condition-check-plugin"
+        "standard-pre-condition-check-plugin"
 
     override fun getName() =
-            "Standard pre condition check"
+        "Standard pre condition check"
 
     override fun getVersion(): String =
-            "V1.0.0"
+        "1.0.0"
 }
