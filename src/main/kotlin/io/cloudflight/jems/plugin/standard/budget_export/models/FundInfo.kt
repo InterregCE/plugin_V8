8dc654c0a2853f48748654825476d2965ee98b8c
Mehran Haghani <Mehran.Haghani@cloudflight.io>
2021-12-20 10:03:59 +0100

    MP2-1728 - adds percentage of total to each fund

--- src/main/kotlin/io/cloudflight/jems/plugin/standard/budget_export/models/FundInfo.kt
+++ src/main/kotlin/io/cloudflight/jems/plugin/standard/budget_export/models/FundInfo.kt
@@ -2,7 +2,7 @@ package io.cloudflight.jems.plugin.standard.budget_export.models
 
 import java.math.BigDecimal
 
-data class FundInfo(val fundAmount: BigDecimal?, val fundPercentage: BigDecimal?) {
+data class FundInfo(val fundAmount: BigDecimal?, val fundPercentage: BigDecimal?, val percentageOfTotal: BigDecimal?) {
     fun toStringList() =
-        listOf((fundAmount ?: BigDecimal.ZERO).toString(), (fundPercentage ?: BigDecimal.ZERO).toString())
+        listOf((fundAmount ?: BigDecimal.ZERO).toString(), (fundPercentage ?: BigDecimal.ZERO).toString(), (percentageOfTotal ?: BigDecimal.ZERO).toString())
 }
